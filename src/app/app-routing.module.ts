5070edfc4c5f1f6d30e50f2714a2d164325486ed
t-surendra <tsurendra@sinepal.com>
2022-03-10 16:25:49 +0545

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewsComponent } from './pages/news/news.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
